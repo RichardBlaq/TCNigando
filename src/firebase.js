4014fbd744bc184b559feb8e972abac7abbf4d89
RichardBlaq <ririchwords@gmail.com>
2025-06-02 13:32:50 +0100

    Fix import case for auth component

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,6 @@
-
-
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
 import { getAuth, updateProfile } from 'firebase/auth' // Import updateProfile here
 import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore'
 
@@ -12,24 +10,23 @@ import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBMwIyYGhQxtzcRn1gv0DOzQF7Gwnyua-g",
-  authDomain: "covenantsite.firebaseapp.com",
-  projectId: "covenantsite",
-  storageBucket: "covenantsite.firebasestorage.app",
-  messagingSenderId: "723696715602",
-  appId: "1:723696715602:web:03aba63bb3e398a3651c8b",
-  measurementId: "G-3RCBEV88RP"
-};
+  apiKey: 'AIzaSyBMwIyYGhQxtzcRn1gv0DOzQF7Gwnyua-g',
+  authDomain: 'covenantsite.firebaseapp.com',
+  projectId: 'covenantsite',
+  storageBucket: 'covenantsite.firebasestorage.app',
+  messagingSenderId: '723696715602',
+  appId: '1:723696715602:web:03aba63bb3e398a3651c8b',
+  measurementId: 'G-3RCBEV88RP',
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const db = getFirestore(app);
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+const auth = getAuth(app)
+const db = getFirestore(app)
 
 console.log('Firebase app initialized:', app)
 console.log('Auth instance:', auth)
-export { auth, db, updateProfile }; // Export updateProfile
-
+export { auth, db, updateProfile } // Export updateProfile
 
 // Export auth for use in other files
